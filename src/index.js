6e6435a5d8e8bb482bf33c9c86c266c37b78d7ee
artnuvo <215204687+artnuvo@users.noreply.github.com>
2026-02-18 08:20:16 +0100

    Redirect tribecamp.org to coliven.com event page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    
+
+    // Redirect tribecamp.org to coliven.com event page
+    if (url.hostname === 'tribecamp.org' || url.hostname === 'www.tribecamp.org') {
+      return Response.redirect('https://coliven.com/events/c25ab192e5ec', 301);
+    }
+
     // Serve index.html for root path
     if (url.pathname === '/' || url.pathname === '/index.html') {
       return env.ASSETS.fetch(request);
     }
-    
+
     // Try to serve static assets
     try {
       return await env.ASSETS.fetch(request);
